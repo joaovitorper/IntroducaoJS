e1b1c29e69c81a8aca5107e8bba2c48a8bc107a2
joaovitorper <jovipepa.10@gmail.com>
2025-06-25 10:11:19 -0300

    arruma o   exercicio 5

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -5,3 +5,25 @@ let somar = parseFloat(nume1)+parseFloat(nume2);
 document.getElementById("resultado").innerHTML = "resultado: " + somar;
 }
 
+function subtracao(){
+    let nume1 = document.getElementById("num1").value;
+    let nume2 = document.getElementById("num2").value;
+    let subtração = parseFloat(nume1)-parseFloat(nume2);
+    document.getElementById("resultado").innerHTML = "resultado: " + subtração;
+    }
+    
+
+    function multiplicacao(){
+        let nume1 = document.getElementById("num1").value;
+        let nume2 = document.getElementById("num2").value;
+        let multiplicacao = parseFloat(nume1)*parseFloat(nume2);
+        document.getElementById("resultado").innerHTML = "resultado: " + multiplicacao;
+        }
+        
+        function divisao(){
+            let nume1 = document.getElementById("num1").value;
+            let nume2 = document.getElementById("num2").value;
+            let divisao = parseFloat(nume1)/parseFloat(nume2);
+            document.getElementById("resultado").innerHTML = "resultado: " + divisao;
+            }
+            
